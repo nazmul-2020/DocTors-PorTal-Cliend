f115c32beb23c76fb84aa8ec2ee5e1f73028159a
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-05-10 18:12:50 +0600

    all menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
+import Contact from "./Pages/Contact/Contact";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Reviews from "./Pages/Reviews/Reviews";
 import Navbar from "./Pages/Shared/Navbar/Navbar";
 
 
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="about" element={<About></About>}></Route>
+        <Route path="/appointment" element={<Appointment></Appointment>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
