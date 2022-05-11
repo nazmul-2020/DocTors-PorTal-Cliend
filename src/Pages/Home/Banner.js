bad7dc1e0735c48735a6474527915fdc51aa2115
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-05-11 14:52:38 +0600

    Make Info Card Dynamic with dynamic CSS Class

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import chair from "../../assets/images/chair.png"
+import bg from "../../assets/images/bg.png"
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen ">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+        <div className="hero min-h-screen px-12" style={{backgroundImage:`url(${bg})`}} >
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-md rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                    <button className="btn btn-primary uppercase text-white bg-gradient-to-r from-secondary to-primary">Get Started</button>
                 </div>
             </div>
         </div>
