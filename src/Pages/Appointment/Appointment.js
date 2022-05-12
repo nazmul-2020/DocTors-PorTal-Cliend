d7a567e191f9ad6dcbf5f2ece7c369db706576c0
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-05-12 11:16:22 +0600

    start Appointment page

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Footer from '../Home/Footer';
+import AppointmentBanner from './AppointmentBanner';
 
 const Appointment = () => {
     return (
         <div>
-            <h1>Appointment</h1>
+            <AppointmentBanner></AppointmentBanner>
+            <Footer></Footer>
+            
         </div>
     );
 };
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -9,7 +9,7 @@ const MakeAppointment = () => {
         }} 
         className='flex justify-center items-center'>
             <div className='flex-1 hidden lg:block'>
-                <img className='mt-[-100px]' src={doctor} alt="" />
+                <img className='mt-[-160px]' src={doctor} alt="" />
             </div>
             <div className='flex-1'>
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
